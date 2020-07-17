a16feb51b23db8494024c954fdacae17f2ec1819
Gormley <Mark.Gormley@bsci.com>
2020-07-17 21:04:32 +0100

    5.24 create post model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,9 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
+const axios = require("axios");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -315,6 +318,27 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (error) {
     console.error(err.message);
     res.status(500).send("Server Error");
+  }
+});
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      "user-agent": "node.js",
+      Authorization: `token ${config.get("githubToken")}`,
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: "No Github profile found" });
   }
 });
 
